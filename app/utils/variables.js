006afe36594874a8758ccb8c84d8ef27ffa73c6e
wwervin72 <2411120974@qq.com>
2020-04-30 00:00:19 +0800

    migration

--- app/utils/variables.js
+++
@@ -1,7 +0,0 @@
-exports.userNameReg = /^[a-zA-Z0-9]{5,16}$/
-exports.userNameTips = '账号需为5到16个长度的字母或数字'
-
-exports.passWordReg = /^[a-zA-Z0-9-_.]{5,20}$/
-exports.passWordTips = '密码必须是长度为5到20个的字母、数字、-、_、.'
-
-exports.fullPathReg = /^(https?:)?\/\//
